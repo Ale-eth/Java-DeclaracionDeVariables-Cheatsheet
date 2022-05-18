1321039c7609da86f257952e16a8a8af8bb17dd9
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-05-18 16:14:32 -0300

    Update Declaracion.java

--- Declaracion.java
+++ Declaracion.java
@@ -1,1 +1,14 @@
 
+// ARREGLOS:
+
+// Para declarar un arreglo vacio:
+tipoDeDato [] nombreArreglo;
+int [] numeros;
+
+// Para declarar un arreglo de determinadas posiciones:
+nombreArreglo = new tipoDeDato[CANTIDAD];
+numeros = new int[10];
+
+// Para declarar un arreglo con las posiciones explicitas:
+nombreArreglo = {valor1, valor2, valor3, valorN};
+numeros = {1, 2, 3, 4, 5};
