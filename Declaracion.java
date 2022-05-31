ef396896d127cbb2cba67e9ae40f5d5b551d0501
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-05-31 15:22:56 -0300

    Update Declaracion.java

--- Declaracion.java
+++ Declaracion.java
@@ -12,3 +12,6 @@ numeros = new int[10];
 // Para declarar un arreglo con las posiciones explicitas:
 nombreArreglo = {valor1, valor2, valor3, valorN};
 numeros = {1, 2, 3, 4, 5};
+
+//
+int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
